7257762523fb86629c9c7857cf144241390687ab
jjsk109 <jjsk109@gmail.com>
2021-10-06 08:42:03 +0900

    first PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>문서의 제목 </h1>
+    <?php 
+        
+        $name = "장익순";
+        echo '반갑습니다'. $name;
+        echo "굉장히 반갑습니다. $name"
+    ?>
+</body>
+</html>
